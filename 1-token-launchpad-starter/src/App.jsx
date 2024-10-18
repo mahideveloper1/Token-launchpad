ba2ceb4b7a69020e4bccc19c9ef905e641607ad8
Mahesh Vashisth <maheshvashisth07@gmail.com>
2024-10-18 16:46:08 +0530

    added solana adapter

--- 1-token-launchpad-starter/src/App.jsx
+++ 1-token-launchpad-starter/src/App.jsx
@@ -1,10 +1,37 @@
-import './App.css'
-import { TokenLaunchpad } from './components/TokenLaunchpad'
+import "./App.css";
+import { TokenLaunchpad } from "./components/TokenLaunchpad";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 function App() {
   return (
-    <TokenLaunchpad></TokenLaunchpad>
-  )
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton></WalletMultiButton>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: 20,
+            }}
+          >
+            <WalletDisconnectButton />
+          </div>
+          {/* maincode in TokenLaunchpad */}
+          <TokenLaunchpad></TokenLaunchpad>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
-export default App
+export default App;
--- 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,17 +1,55 @@
+import { createMint } from "@solana/spl-token";
 
 export function TokenLaunchpad() {
-    return  <div style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
-    }}>
-        <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
-        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+  function createToken() {
+    const name = document.getElementById("name").value;
+    const symbol = document.getElementById("symbol").value;
+    const imageUrl = document.getElementById("imageUrl").value;
+    const intialSupply = document.getElementById("intialSupply").value;
+  }
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Solana Token Launchpad</h1>
+      <input
+        id="name"
+        className="inputText"
+        type="text"
+        placeholder="Name"
+      ></input>{" "}
+      <br />
+      <input
+        id="symbol"
+        className="inputText"
+        type="text"
+        placeholder="Symbol"
+      ></input>{" "}
+      <br />
+      <input
+        id="imageUrl"
+        className="inputText"
+        type="text"
+        placeholder="Image URL"
+      ></input>{" "}
+      <br />
+      <input
+        id="initialSupply"
+        className="inputText"
+        type="text"
+        placeholder="Initial Supply"
+      ></input>{" "}
+      <br />
+      <button onClick={createToken} className="btn">
+        Create a token
+      </button>
     </div>
+  );
 }
